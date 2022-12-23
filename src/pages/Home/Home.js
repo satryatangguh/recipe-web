8dd5dcab2231d81783f7db8b564b325f9888e65c
Satrya Tangguh <satryatp@gmail.com>
2022-12-24 05:19:11 +0700

    Update card height and styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -78,61 +78,59 @@ const Home = () => {
   return (
     <>
       <Carousel />
-      <section className="container-fluid px-3 py-5">
+      <section className="container-fluid py-5">
         <h1 className="title text-center">Explore Our Recipes</h1>
-        <div className="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4 mx-3">
+        <div className="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4 mt-3 mx-5">
           {food &&
             food.map((r) => {
               return (
-                <div className="card-group" key={r.id}>
-                  <div className="card h-100 shadow mt-3">
+                <div className="card-group gy-0" key={r.id}>
+                  <div className="card mh-100 shadow mt-4">
                     <img
                       src={r.imageUrl}
                       className="card-img-top mx-auto card-image"
                       alt={r.name}
                     />
-                    <div className="card-body">
-                      <h5 className="card-title text-center text-capitalize fs-5 mb-4">
+                    <div className="card-body d-flex flex-column p-2">
+                      <h5 className="card-title text-start text-capitalize fs-6 mb-1">
                         {r.name}
                       </h5>
-                      <p className="card-text fs-6 mb-1 fw-bold">
-                        Desc: <span className="fw-normal">{r.description}</span>
-                      </p>
-                      <p className="card-text fs-6 mb-1 fw-bold">
-                        Ingredients:{" "}
-                        {r.ingredients.map((m, index) => {
-                          return (
-                            <span className="fw-normal" key={index}>
-                              {(index ? ", " : " ") + m}
-                            </span>
-                          );
-                        })}
-                      </p>
+                      <div className="d-flex align-items-center mt-auto">
+                        <span className="text-muted d-flex align-items-center me-3 rate">
+                          <Link
+                            style={{ textDecoration: "none" }}
+                            to={`/rating/${r.id}`}
+                          >
+                            <i className="ri-star-fill me-1"></i>
+                          </Link>
+                          {r.rating}
+                        </span>
+                        <span className="text-muted d-flex align-items-center rate">
+                          <i
+                            className="ri-heart-fill me-1"
+                            style={{
+                              color: `${r.isLike ? "red" : "gray"}`,
+                            }}
+                            onClick={() => handleLike(r.id, r.isLike)}
+                          ></i>
+                          {r.totalLikes}
+                        </span>
+                      </div>
                     </div>
-                    <div className="card-footer">
-											<div className="d-flex align-items-center">
-												<span className="text-muted d-flex align-items-center me-3">
-													<Link style={{textDecoration: "none"}} to={`/rating/${r.id}`}>
-														<i className="ri-star-fill me-1"></i>
-													</Link>
-													{r.rating}
-												</span>
-												<span className="text-muted d-flex align-items-center">
-													<i
-														className="ri-heart-fill me-1"
-														style={{
-															color: `${r.isLike ? "red" : "gray"}`,
-														}}
-														onClick={() => handleLike(r.id, r.isLike)}
-													></i>
-													{r.totalLikes}
-												</span>
-											</div>
+                    <div className="card-footer d-flex align-items-center justify-content-end">
+                      <Link
+                        style={{ textDecoration: "none", fontSize: "13px" }}
+                        to={`/detail/${r.id}`}
+                        className="d-flex align-items-center text-success"
+                      >
+                        View Detail
+                        <i className="ri-arrow-right-line ms-1"></i>
+                      </Link>
                     </div>
                   </div>
                 </div>
               );
-            })}
+          })}
         </div>
       </section>
     </>
